11a0f6b20dafa18906ee3bb470a69171b91889e6
IdaGreen1 <163199429+IdaGreen1@users.noreply.github.com>
2024-05-22 22:38:00 +0200

    Update script.js

--- JavaScript/Code/05 - Working With Text/Task/script.js
+++ JavaScript/Code/05 - Working With Text/Task/script.js
@@ -1,6 +1,23 @@
 var text = ' Lorem ipsum dolor sit amet';
+console.log(text)
+
+//1
 var textLength = text.length;
 console.log(textLength);
 
-var textSit = text.slice(indexOfWord + 3);
+//2
+var textSit = text.slice(20, 22);
 console.log(textSit);
+
+//3
+text.replace('amet', 'elit');
+
+//4
+var concat = text + 'consectetur adipiscing elit';
+
+//5
+text.toLocaleUpperCase;
+
+//6
+text.
+
