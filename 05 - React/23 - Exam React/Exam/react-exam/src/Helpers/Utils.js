2975887041a9e07d7cc5d3167fdf225956ec8d15
IdaGreen1 <163199429+IdaGreen1@users.noreply.github.com>
2024-10-24 20:48:04 +0200

    Update Utils.js

--- 05 - React/23 - Exam React/Exam/react-exam/src/Helpers/Utils.js
+++ 05 - React/23 - Exam React/Exam/react-exam/src/Helpers/Utils.js
@@ -2,6 +2,6 @@ export const options = {
     method: 'GET',
     headers: {
         accept: 'application/json',
-        authorization: 'Bearer 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'
+        authorization: 'Bearer '
     },
 };
